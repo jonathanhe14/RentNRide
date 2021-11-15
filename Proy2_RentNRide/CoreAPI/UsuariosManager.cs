1a86e181c47eeea3fd3a5e20705444729fc5a581
Jonathan Herrera Castillo <jherrerac@ucenfotec.ac.cr>
2021-11-14 21:12:50 -0600

    Registro Usuario Final

---
+++ Proy2_RentNRide/CoreAPI/UsuariosManager.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Crud;
+using Entities_POJO;
+using Exceptions;
+
+namespace CoreAPI
+{
+    public class UsuariosManagement : BaseEntity
+    {
+        private UsuariosCrudFactory crudUsuarios;
+        private ContrasennaCrudFactory crudContrasennas;
+
+        public UsuariosManagement()
+        {
+            crudUsuarios = new UsuariosCrudFactory();
+            crudContrasennas = new ContrasennaCrudFactory();
+        }
+
+        public void Create(Usuarios usuario)
+        {
+            try
+            {
+
+                //Valida si ya existe
+                var u = crudUsuarios.Retrieve<Usuarios>(usuario);
+                if (u != null)
+                {
+                    throw new BussinessException(1);
+                }
+                //Valida si es mayor de edad
+                if (usuario.Edad < 18)
+                {
+                    throw new BussinessException(2);
+
+                }
+                usuario.Estado = "Pendiente";
+                usuario.OTP = 1;
+                usuario.Latitud = "123.313123";
+                usuario.Longitud = "123.313123";
+                usuario.PersoneriaJuridica = "NULL";
+                usuario.PermisoOperaciones = "NULL";
+
+                var contrasenna = new Contrasennas
+                {
+                    Contrasenna = usuario.ContrassenaActual,
+                    Correo = usuario.Correo,
+                    Fecha = DateTime.Now
+                };
+
+
+                //Validacion del Rol y de los campos necesarios
+
+
+                crudUsuarios.Create(usuario);
+                crudContrasennas.Create(contrasenna);
+            }
+            catch (Exception ex)
+            {
+                ExceptionManager.GetInstance().Process(ex);
+            }
+        }
+
+        public List<Usuarios> RetrieveAll()
+        {
+            try
+            {
+                return crudUsuarios.RetrieveAll<Usuarios>();
+            }
+            catch (Exception ex)
+            {
+                throw new BussinessException(0);
+            }
+
+        }
+
+        public Usuarios RetrieveById(Usuarios usuarios)
+        {
+            try
+            {
+                return crudUsuarios.Retrieve<Usuarios>(usuarios);
+            }
+            catch (Exception ex)
+            {
+                throw new BussinessException(0);
+            }
+        }
+
+        public void Update(Usuarios usuarios)
+        {
+            try
+            {
+                crudUsuarios.Update(usuarios);
+            }
+            catch (Exception ex)
+            {
+                throw new BussinessException(0);
+            }
+        }
+
+    }
+}
---
+++ Proy2_RentNRide/DataAccess/Crud/UsuariosRolCrudFactory.cs
@@ -0,0 +1,74 @@
+﻿using DataAccess.Dao;
+using DataAccess.Mapper;
+using Entities_POJO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Crud
+{
+    public class UsuariosRolCrudFactory : CrudFactory
+    {
+        UsuariosRolMapper mapper;
+
+        public UsuariosRolCrudFactory() : base()
+        {
+            mapper = new UsuariosRolMapper();
+            dao = SqlDao.GetInstance();
+        }
+
+        public override void Create(BaseEntity entity)
+        {
+            var usuarios = (Usuarios)entity;
+            var sqlOperation = mapper.GetCreateStatement(usuarios);
+            dao.ExecuteProcedure(sqlOperation);
+        }
+
+        public override T Retrieve<T>(BaseEntity entity)
+        {
+            var sqlOperation = mapper.GetRetriveStatement(entity);
+            var lstResult = dao.ExecuteQueryProcedure(sqlOperation);
+            var dic = new Dictionary<string, object>();
+            if (lstResult.Count > 0)
+            {
+                dic = lstResult[0];
+                var objs = mapper.BuildObject(dic);
+                return (T)Convert.ChangeType(objs, typeof(T));
+            }
+
+            return default(T);
+        }
+
+        public override List<T> RetrieveAll<T>()
+        {
+            var lstUsuariosRoles = new List<T>();
+
+            var lstResult = dao.ExecuteQueryProcedure(mapper.GetRetriveAllStatement());
+            var dic = new Dictionary<string, object>();
+            if (lstResult.Count > 0)
+            {
+                var objs = mapper.BuildObjects(lstResult);
+                foreach (var c in objs)
+                {
+                    lstUsuariosRoles.Add((T)Convert.ChangeType(c, typeof(T)));
+                }
+            }
+
+            return lstUsuariosRoles;
+        }
+
+        public override void Update(BaseEntity entity)
+        {
+            var usuarios = (Usuarios)entity;
+            dao.ExecuteProcedure(mapper.GetUpdateStatement(usuarios));
+        }
+
+        public override void Delete(BaseEntity entity)
+        {
+            var usuarios = (Usuarios)entity;
+            dao.ExecuteProcedure(mapper.GetDeleteStatement(usuarios));
+        }
+    }
+}
