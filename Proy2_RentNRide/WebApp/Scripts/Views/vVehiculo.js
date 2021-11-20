8de2849f9e1e7fa7e829c496ed9308ab83390df9
TomasChVa <tchavarriav@ucenfotec.ac.cr>
2021-11-20 10:23:47 -0600

    Google map creado para visualisar ubicacion

--- Proy2_RentNRide/WebApp/Scripts/Views/vVehiculo.js
+++ Proy2_RentNRide/WebApp/Scripts/Views/vVehiculo.js
@@ -4,12 +4,19 @@
 	this.service = 'vehiculo';
 	this.ctrlActions = new ControlActions();
 
+	var markers = [];
+
+	var posiciones = [];
+
+	var map;
+
 	this.Create = function () {
 		var customerData = {};
 		var idData = {};
 		var documentoData = {};
+		var UserOption = document.getElementById('drpTipoVehi').value;
 		idData = this.ctrlActions.GetDataForm('idEdition');
-		customerData = idData + this.ctrlActions.GetDataForm('frmEdition');
+		customerData = idData + UserOption + this.ctrlActions.GetDataForm('frmEdition');
 		documentoData = idData + this.ctrlActions.GetDataForm('docuEdition');
 		console.log(customerData);
 		console.log(documentoData);
@@ -29,10 +36,47 @@
 
 	}
 
+	function initMap() {
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: { lat: 9.932781, lng: -84.031195 },
+			zoom: 18
+		});
+	}
+
+		function crearMarcador(position) {
+			posiciones.push(posiciones);
+			console.log(posiciones.length)
+
+			const marker = new google.maps.Marker({
+				position,
+				map,
+			});
+
+			markers.push(marker);
+
+			//centrar el mapa a la posicion del parametro
+			map.setCenter(position);
+
+			console.log("Marcador creado");
+		}
+
+		function buscarUbicacion(lat, lon) {
+			latitud = parseFloat(lat);
+			longitud = parseFloat(lon);
+
+			map.setCenter(new google.maps.LatLng(latitud, longitud));
+			markers = { lat: latitud, lng: longitud };
+			marker = new google.maps.Marker({
+				position: markers,
+				map: map,
+			});
+		}
+
 
 	this.BindFields = function (data) {
 		this.ctrlActions.BindFields('frmEdition', data);
 	}
+	
 
 }
 
