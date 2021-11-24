6595456391f00f785c77ec09c65e64edfc952d79
Andy0910 <andres.gonzalez42@hotmail.com>
2021-11-23 19:07:40 -0600

    commit con avance en admin

--- Proy2_RentNRide/WebApp/Scripts/Views/Administrador.js
+++ Proy2_RentNRide/WebApp/Scripts/Views/Administrador.js
@@ -9,6 +9,10 @@
 	this.usersService = 'usuarios';
 	this.ctrlActions = new ControlActions();
 	this.usrChartId = 'usrChart';
+	this.usrAdmin = 'lblAdmin';
+	this.usrFinales = 'lblFinales';
+	this.usrSocios = 'lblSocios';
+	this.usrEmpresas = 'lblEmpresas';
 
 
 	this.RetrieveAll = function () {
@@ -221,16 +225,44 @@
 	}
 
 	// Usuarios
-	this.GetDataToChart = function (initializeChartFunction) {
+	/*this.GetDataToChart = function (initializeChartFunction) {
 
 		this.ctrlActions.GetToApi(this.service + '/Get', initializeChartFunction);
-	};
+	};*/
+
+	this.RetrieveAllUsuarios = function () {
+		this.ctrlActions.GetToApi(this.usersService + '/GetRoles', function (data) {
+			var admin = 0;
+			var empresas = 0;
+			var socios = 0;
+			var finales = 0;
+			
+			for (var i = 0; i < data.length; i++) {
+				if (data[i]["IdRol"] == 1) {
+					admin - admin + 1;
+				} else if (data[i]["IdRol"] == 2) {
+					socios = socios + 1;
+				} else if (data[i]["IdRol"] == 3) {
+					empresas = empresas + 1;
+				} else {
+					finales = finales + 1;
+				}
+			}
+
+			$("#lblAdmin").text(admin);
+			$("#lblEmpresas").text(empresas);
+			$("#lblSocios").text(socios);
+			$("#lblFinales").text(finales);
+
+		});
+		
+    }
 }
 
 //ON DOCUMENT READY
 $(document).ready(function () {
 	var administrador = new Administrador();
 	administrador.RetrieveAll();
-
+	administrador.RetrieveAllUsuarios();
 });
 
--- Proy2_RentNRide/WebApp/Scripts/Views/Solicitudes.js
+++ Proy2_RentNRide/WebApp/Scripts/Views/Solicitudes.js
@@ -2,29 +2,80 @@
 
 	this.tblUsuariosId = 'tblUsuarios';
 	this.usersService = 'usuarios';
+	this.adminService = 'administrador';
+	this.tblMembresiasId = 'tblMembresias'
 	this.ctrlActions = new ControlActions();
 
 
 	this.RetrieveAll = function () {
 		this.ctrlActions.FillTable(this.usersService + "/GetSolicitudes", this.tblUsuariosId, false);
+		this.ctrlActions.FillTable(this.adminService + "/GetMembresias", this.tblMembresiasId, false);
 	}
 
 	this.ReloadTable = function () {
 		this.ctrlActions.FillTable(this.usersService + "/GetSolicitudes", this.tblUsuariosId, true);
+		this.ctrlActions.FillTable(this.adminService + "/GetMembresias", this.tblMembresiasId, true);
 	}
 
 	this.Rechazar = function () {
-
+		var usuario = {};
+		usuario = this.ctrlActions.GetDataForm('frmUsers');
+		//Hace el post al create
+		this.ctrlActions.PostToAPI(this.usersService + "/Rechazar", usuario, function (data) {
+			var solicitudes = new Solicitudes();
+			solicitudes.ReloadTable();
+		});
 	}
 
 	this.Aceptar = function () {
+		var usrVacio = false;
+		var membVacio = false;
+		$('#frmUsers input[type="text"]').each(function () {
+			if ($(this).val() == "") {
+				usrVacio = true;
+			}
+		});
 
+		$('#frmMembresia input[type="text"]').each(function () {
+			if ($(this).val() == "") {
+				membVacio = true;
+			}
+		});
+
+		if (usrVacio || membVacio) {
+
+		} else {
+			var usuario = {};
+			usuario = this.ctrlActions.GetDataForm('frmUsers');
+			var membresia = {};
+			membresia = this.ctrlActions.GetDataForm('frmMembresia');
+			//Hace el post al create
+			this.ctrlActions.PostToAPI(this.usersService + "/Aceptar/"+membresia.Id, usuario, function (data) {
+				var solicitudes = new Solicitudes();
+				solicitudes.ReloadTable();
+			});
+        }
+
+
+	}
+
+	this.Limpiar = function () {
+		$('#frmUsers input[type="text"]').each(function () {
+			$(this).val("");
+		});
+
+		$('#frmMembresia input[type="text"]').each(function () {
+			$(this).val("");
+		});
     }
 
 	this.BindFieldsUsers = function (data) {
 		this.ctrlActions.BindFields('frmUsers', data);
 	}
 
+	this.BindFieldsMembresia = function (data) {
+		this.ctrlActions.BindFields('frmMembresia', data);
+	}
 
 }
 
